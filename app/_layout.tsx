e47c2e481720a9de5e338fce348ac2ebf5cbedd1
Ikamaz <mazmishviliika@gmail.com>
2025-01-27 20:18:57 +0400

    commit5

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,13 +6,18 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/constants/Colors';
+
+import { Appearance } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = Appearance.getColorScheme()
+
+  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light;
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -28,15 +33,9 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack screenOptions={{ headerStyle: { backgroundColor: theme}}}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="(coffee)" options={{ headerShown: false }} /> */}
-        {/* <Stack.Screen name='index' options={{ title: 'Home', headerShown: false }}></Stack.Screen>
-        <Stack.Screen name='contact' options={{ title: 'Contact' }}></Stack.Screen> */}
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
   );
 }
